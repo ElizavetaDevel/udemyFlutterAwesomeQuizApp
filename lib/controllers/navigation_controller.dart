a13c1ab870446ed95c97a2d625b11ba99d7f186a
Eliza A <elizaveta.devel@gmail.com>
2023-11-26 02:21:21 +0800

    store active screen as enum instead of widget

--- lib/controllers/navigation_controller.dart
+++ lib/controllers/navigation_controller.dart
@@ -3,6 +3,11 @@ import 'package:awesome_quiz_app/controllers/home_controller.dart';
 import 'package:awesome_quiz_app/controllers/quiz_controller.dart';
 import 'package:flutter/material.dart';
 
+enum Screen {
+  home,
+  quiz,
+}
+
 class NavigationController extends StatefulWidget {
   const NavigationController({super.key});
 
@@ -16,24 +21,27 @@ class _NavigationControllerState extends State<NavigationController> {
     Color.fromARGB(255, 34, 4, 86)
   ];
 
-  Widget? activeScreen;
+  Screen activeScreen = Screen.home;
 
   void onStartPressed() {
     setState(() {
-      activeScreen = const QuizController();
+      activeScreen = Screen.quiz;
     });
   }
 
-  @override
-  void initState() {
-    activeScreen = HomeController(onStartPressed);
-    super.initState();
+  Widget displayScreen() {
+    switch (activeScreen) {
+      case Screen.home:
+        return HomeController(onStartPressed);
+      case Screen.quiz:
+        return const QuizController();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GradientContainer(activeScreen, colors),
+      body: GradientContainer(displayScreen(), colors),
     );
   }
 }
