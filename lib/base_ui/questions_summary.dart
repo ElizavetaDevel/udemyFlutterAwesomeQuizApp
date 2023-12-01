d975787249e8f0b46f45ee26facadb60cdc0da11
Eliza A <elizaveta.devel@gmail.com>
2023-12-01 23:53:33 +0800

    fix ui

--- lib/base_ui/questions_summary.dart
+++ lib/base_ui/questions_summary.dart
@@ -13,65 +13,65 @@ class QuestionsSummary extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 32,
-                      height: 32,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: (data['user_answer'] == data['correct_answer'])
-                              ? Colors.greenAccent
-                              : Colors.redAccent),
-                      child: Center(
-                        child: StyledText(
-                          ((data['question_index'] as int) + 1).toString(),
-                          16,
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (data['user_answer'] == data['correct_answer'])
+                            ? Colors.greenAccent
+                            : Colors.redAccent),
+                    child: Center(
+                      child: StyledText(
+                        ((data['question_index'] as int) + 1).toString(),
+                        16,
+                        Colors.deepPurple,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledText(
+                          data['question'] as String,
+                          14,
                           Colors.white,
                           fontWeight: FontWeight.bold,
+                          textAlign: TextAlign.left,
                         ),
-                      ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        StyledText(
+                          data['user_answer'] as String,
+                          12,
+                          Colors.redAccent,
+                          textAlign: TextAlign.left,
+                        ),
+                        StyledText(
+                          data['correct_answer'] as String,
+                          12,
+                          Colors.greenAccent,
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          StyledText(
-                            data['question'] as String,
-                            14,
-                            Colors.white,
-                            fontWeight: FontWeight.bold,
-                            textAlign: TextAlign.left,
-                          ),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          StyledText(
-                            data['user_answer'] as String,
-                            12,
-                            Colors.redAccent,
-                            textAlign: TextAlign.left,
-                          ),
-                          StyledText(
-                            data['correct_answer'] as String,
-                            12,
-                            Colors.greenAccent,
-                            textAlign: TextAlign.left,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             );
           }).toList(),
         ),
